82ce5a13aeb0ffe3324dae627d60ec1d3d28fd0e
Idriss <ihippocrate@gmail.com>
2024-01-22 10:09:50 +0100

    fix parser

--- src/modules/message/services/MessageDataParser.ts
+++ src/modules/message/services/MessageDataParser.ts
@@ -39,56 +39,7 @@ export class MessageDataParser {
       /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/im;
 
     const attachements: MessageAttachement[] = [];
-
-    for (const token of text.tokens) {
-      if (token.type === "link") {
-        const pictureMatch = pictureRegex.exec(token.value);
-
-        if (pictureMatch) {
-          attachements.push({
-            type: "image",
-            src: token.value
-          });
-        }
-
-        const videoMatch = videoRegex.exec(token.value);
-
-        if (videoMatch) {
-          attachements.push({
-            type: "video",
-            src: token.value
-          });
-        }
-
-        const audioMatch = audioRegex.exec(token.value);
-
-        if (audioMatch) {
-          attachements.push({
-            type: "audio",
-            src: token.value
-          });
-        }
-
-        const youtubeMatch = youtubeRegex.exec(token.value);
-
-        if (youtubeMatch) {
-          console.log(youtubeMatch);
-          attachements.push({
-            type: "youtube",
-            videoId: youtubeMatch[2],
-            domain: "youtube.com"
-          });
-        }
-
-        if (!pictureMatch && !videoMatch && !audioMatch && !youtubeMatch) {
-          attachements.push({
-            type: "website",
-            url: token.value
-          });
-        }
-      }
-    }
-
+    // TODO
     return attachements;
   }
 }
